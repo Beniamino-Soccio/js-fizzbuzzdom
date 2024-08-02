7873e5dbdd7e59dda0184c29e678d2e82ea1959a
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-02 16:01:00 +0200

    exercise phases

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//SCALETTA TRACCIA
+// MILESTONE 1
+/*
+-Creo un ciclo che mi "conti" i numeri a partire da 1 fino a 100
+-stampare in console i numeri
+-calcolo se un numero è divisibile per 3 e al posto del numero ci stampo Fizz
+-calcolo se un numero è divisibile per 5 e al posto del numero ci stampo Buzz
+-calcolo se un numero è divisibile per 3 e per 5 e al posto del numero ci stampo FizzBuzz
+*/
+//milestone 2
+/* 
+-creiamomo un elemento contenitore nel DOM
+-recuperiamo l'elemento dal DOM
+-stampiamo i numeri all'interno dell ' elemento nel DOM
+*/
+//milestone 3
+/* 
+-personalizzare la pagina con css
+*/
